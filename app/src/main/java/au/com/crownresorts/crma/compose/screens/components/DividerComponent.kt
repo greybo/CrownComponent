3aa9555e588d863b5a38ba99e201a7a92bb5bfc7
greybo <sergeybotl@gmail.com>
2023-03-14 19:27:11 +0200

    fixed divider component

--- app/src/main/java/au/com/crownresorts/crma/compose/screens/components/DividerComponent.kt
+++ app/src/main/java/au/com/crownresorts/crma/compose/screens/components/DividerComponent.kt
@@ -1,17 +1,17 @@
 package au.com.crownresorts.crma.compose.screens.components
 
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
+import androidx.compose.material3.Divider
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.Dp
+import au.com.crownresorts.crma.compose.theme.CrownTheme
 
 @Composable
 fun DividerComponent(modifier: Modifier = Modifier) {
-    Spacer(
-        modifier = modifier
-            .fillMaxWidth()
-            .height(0.5.dp)
-    )
+    Divider(color = CrownTheme.colors.divider, thickness = Dp.Hairline)
+//    Spacer(
+//        modifier = modifier
+//            .fillMaxWidth()
+//            .height(0.5.dp)
+//    )
 }
