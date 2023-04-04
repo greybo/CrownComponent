ae2d5dd4c930c667f91afbb903c4f697e1115d2e
greybo <sergeybotl@gmail.com>
2023-04-04 13:22:16 +0300

    AvailableRewards component 2

--- app/src/main/java/au/com/crownresorts/crma/compose/components/AvailableRewards.kt
+++ app/src/main/java/au/com/crownresorts/crma/compose/components/AvailableRewards.kt
@@ -6,7 +6,6 @@ import androidx.compose.foundation.layout.*
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
@@ -16,34 +15,34 @@ import au.com.crownresorts.crma.compose.theme.CrownTheme
 
 @Composable
 fun AvailableRewards(text: String = "Multi-Level & P4 \nParking") {
-    Row(
-        modifier = Modifier
-            .fillMaxWidth()
-//            .aspectRatio(5f / 1f)
-            .background(color = CrownTheme.colors.goldDefault)
-            .padding(horizontal = 16.dp)
-            .padding(top = 8.dp, bottom = 8.dp),
-        verticalAlignment = Alignment.CenterVertically
-    ) {
-        Box(modifier = Modifier.weight(1f)) {
-            TextWithIcon(text = text)
+    Column {
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .background(color = CrownTheme.colors.goldDefault)
+                .padding(horizontal = 16.dp, vertical = 8.dp)
+        ) {
+            Box(modifier = Modifier.weight(1f)) {
+                TextWithIcon(text = text)
+            }
+            Column(modifier = Modifier.weight(1f)) {
+                Text(
+                    text = "Expires 12:00am",
+                    modifier = Modifier.fillMaxWidth(),
+                    style = MaterialTheme.typography.bodyLarge,
+                    color = CrownTheme.colors.textHigh,
+                    textAlign = TextAlign.End,
+                )
+                Text(
+                    text = "07/10/19",
+                    modifier = Modifier.fillMaxWidth(),
+                    style = MaterialTheme.typography.bodyLarge,
+                    textAlign = TextAlign.End,
+                    color = CrownTheme.colors.textHigh
+                )
+            }
         }
-        Column(modifier = Modifier.weight(1f)) {
-            Text(
-                text = "Expires 12:00am",
-                modifier = Modifier.fillMaxWidth(),
-                style = MaterialTheme.typography.bodyLarge,
-                color = CrownTheme.colors.textHigh,
-                textAlign = TextAlign.End,
-            )
-            Text(
-                text = "07/10/19",
-                modifier = Modifier.fillMaxWidth(),
-                style = MaterialTheme.typography.bodyLarge,
-                textAlign = TextAlign.End,
-                color = CrownTheme.colors.textHigh
-            )
-        }
+        InfoRowTooltip(text = "Lorem ipsum dolor sit amet consecteur expelliarmus.")
     }
 }
 
