d760c62d782123755f00a341e8bb57f68a2f0a79
greybo <sergeybotl@gmail.com>
2023-04-07 12:02:32 +0300

    top bar with search 2

--- app/src/main/java/au/com/crownresorts/crma/compose/toolbar/TopAppBarWithSearch.kt
+++ app/src/main/java/au/com/crownresorts/crma/compose/toolbar/TopAppBarWithSearch.kt
@@ -1,11 +1,13 @@
 package au.com.crownresorts.crma.compose.toolbar
 
 import androidx.compose.animation.Crossfade
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material.icons.filled.Close
 import androidx.compose.material.icons.filled.Search
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
@@ -21,6 +23,7 @@ import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 
+val heightAppBar = 56.dp
 
 @Composable
 fun ExpandableSearchView(
@@ -29,12 +32,10 @@ fun ExpandableSearchView(
     onSearchDisplayClosed: () -> Unit,
     modifier: Modifier = Modifier,
     expandedInitially: Boolean = false,
-    tint: Color = MaterialTheme.colorScheme.onPrimary
 ) {
     val (expanded, onExpandedChanged) = remember {
         mutableStateOf(expandedInitially)
     }
-
 
     Crossfade(targetState = expanded) { isSearchFieldVisible ->
         when (isSearchFieldVisible) {
@@ -44,50 +45,40 @@ fun ExpandableSearchView(
                 onSearchDisplayClosed = onSearchDisplayClosed,
                 onExpandedChanged = onExpandedChanged,
                 modifier = modifier,
-                tint = tint
             )
 
             false -> CollapsedSearchView(
                 onExpandedChanged = onExpandedChanged,
                 modifier = modifier,
-                tint = tint
             )
         }
     }
 }
 
 @Composable
-fun SearchIcon(iconTint: Color) {
-    Icon(
-        imageVector = Icons.Filled.Search,
-//        painter = painterResource(id = R.drawable.ic_search),
-        contentDescription = "search icon",
-        tint = iconTint
-    )
-}
-
-@Composable
 fun CollapsedSearchView(
     onExpandedChanged: (Boolean) -> Unit,
     modifier: Modifier = Modifier,
-    tint: Color = MaterialTheme.colorScheme.onPrimary,
 ) {
 
     Row(
         modifier = modifier
             .fillMaxWidth()
-            .padding(vertical = 2.dp),
+            .height(heightAppBar),
         horizontalArrangement = Arrangement.SpaceBetween,
         verticalAlignment = Alignment.CenterVertically
     ) {
         Text(
             text = "Tasks",
             style = MaterialTheme.typography.titleMedium,
-            modifier = Modifier
-                .padding(start = 16.dp)
+            modifier = Modifier.padding(start = 16.dp)
         )
         IconButton(onClick = { onExpandedChanged(true) }) {
-            SearchIcon(iconTint = tint)
+            Icon(
+                imageVector = Icons.Filled.Search,
+                contentDescription = "search icon",
+                tint = Color.White
+            )
         }
     }
 }
@@ -100,7 +91,6 @@ fun ExpandedSearchView(
     onSearchDisplayClosed: () -> Unit,
     onExpandedChanged: (Boolean) -> Unit,
     modifier: Modifier = Modifier,
-    tint: Color = MaterialTheme.colorScheme.onPrimary,
 ) {
     val focusManager = LocalFocusManager.current
 
@@ -115,8 +105,10 @@ fun ExpandedSearchView(
     }
 
     Row(
-        modifier = modifier.fillMaxWidth(),
-        horizontalArrangement = Arrangement.Start,
+        modifier = modifier
+            .fillMaxWidth()
+            .height(heightAppBar),
+        horizontalArrangement = Arrangement.SpaceBetween,
         verticalAlignment = Alignment.CenterVertically
     ) {
         IconButton(onClick = {
@@ -126,7 +118,7 @@ fun ExpandedSearchView(
             Icon(
                 imageVector = Icons.Default.ArrowBack,
                 contentDescription = "back icon",
-                tint = tint
+                tint = Color.Black
             )
         }
         TextField(
@@ -136,14 +128,22 @@ fun ExpandedSearchView(
                 onSearchDisplayChanged(it.text)
             },
             trailingIcon = {
-                SearchIcon(iconTint = tint)
+                Icon(
+                    imageVector = Icons.Filled.Close,
+                    contentDescription = "search icon",
+                    tint = Color.Black,
+                    modifier = Modifier.clickable {
+                        onExpandedChanged(false)
+                        onSearchDisplayClosed()
+                    }
+                )
             },
             modifier = Modifier
                 .fillMaxWidth()
                 .focusRequester(textFieldFocusRequester),
-            label = {
-                Text(text = "Search", color = tint)
-            },
+//            label = {
+//                Text(text = "Search", color = tint)
+//            },
             keyboardOptions = KeyboardOptions(
                 imeAction = ImeAction.Done
             ),
@@ -156,31 +156,40 @@ fun ExpandedSearchView(
     }
 }
 
+//@Composable
+//fun SearchIcon(iconTint: Color) {
+//    Icon(
+//        imageVector = Icons.Filled.Search,
+//        contentDescription = "search icon",
+//        tint = iconTint
+//    )
+//}
+
 @Preview
 @Composable
 fun CollapsedSearchViewPreview() {
-        Surface(
-            color = MaterialTheme.colorScheme.primary
-        ) {
-            ExpandableSearchView(
-                searchDisplay = "",
-                onSearchDisplayChanged = {},
-                onSearchDisplayClosed = {}
-            )
-        }
-   }
+    Surface(
+        color = MaterialTheme.colorScheme.primary
+    ) {
+        ExpandableSearchView(
+            searchDisplay = "",
+            onSearchDisplayChanged = {},
+            onSearchDisplayClosed = {}
+        )
+    }
+}
 
 @Preview
 @Composable
 fun ExpandedSearchViewPreview() {
-        Surface(
-            color = MaterialTheme.colorScheme.primary
-        ) {
-            ExpandableSearchView(
-                searchDisplay = "",
-                onSearchDisplayChanged = {},
-                expandedInitially = true,
-                onSearchDisplayClosed = {}
-            )
-        }
+    Surface(
+        color = MaterialTheme.colorScheme.primary
+    ) {
+        ExpandableSearchView(
+            searchDisplay = "",
+            onSearchDisplayChanged = {},
+            expandedInitially = true,
+            onSearchDisplayClosed = {}
+        )
+    }
 }
